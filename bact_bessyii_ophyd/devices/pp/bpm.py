63bddd548aa7ccf10a304caa2e9263175ae7cc0a
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2023-11-17 16:25:12 +0100

    [TASK] using bact_device_models now

--- bact_bessyii_ophyd/devices/pp/bpm.py
+++ bact_bessyii_ophyd/devices/pp/bpm.py
@@ -6,7 +6,7 @@ from .bpm_parameters import create_bpm_config
 from bact_bessyii_mls_ophyd.devices.process.bpm_packed_data import packed_data_to_named_array
 import functools
 from ophyd import Component as Cpt, Signal, Kind
-from bact_bessyii_mls_ophyd.devices.utils.bpm_elem import BpmElementList, BpmElemPlane, BpmElem
+from bact_device_models.devices.bpm_elem import BpmElementList, BpmElemPlane, BpmElem
 from ..raw.bpm import BPM as BPMR
 
 
