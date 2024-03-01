e60e42d1eafadd457d0f5fb62fb10b6c65c8045e
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-03-01 19:47:21 +0100

    [TASK] clean up: code that's still needed

--- bact_bessyii_ophyd/devices/pp/steerers/steerer_muxer.py
+++ bact_bessyii_ophyd/devices/pp/steerers/steerer_muxer.py
@@ -1,10 +1,33 @@
+import logging
+
 from bact_bessyii_mls_ophyd.devices.utils.power_converters_as_multiplexer import MultiplexerSetMixin, \
     ScaledPowerConverter, SelectedPowerConverter
+from bact_bessyii_ophyd.devices.interfaces.configuration_repository import ConfigurationRepositoryInterface
 from .steerer_configuration_repository import ConfigurationRepository
 from ophyd import Component as Cpt, Device, DynamicDeviceComponent as DDC, Kind, Signal
 from ophyd.areadetector import ad_group
 
+logger = logging.getLogger("bact-bessyii-ophyd")
 repo = ConfigurationRepository()
+
+
+def configure_scaling_power_converters(power_converters, repository: ConfigurationRepositoryInterface):
+    """set offset and scale for all power converters given by their names
+
+    Args:
+        power_converters:
+        repository:
+    """
+    for name in repo.get_device_names():
+        conf = repo.get(name)
+        try:
+            pc = getattr(power_converters, name)
+        except AttributeError as exc:
+            logger.warning("Could not configure device %s: reason: %s", name, exc)
+            continue
+
+        d = dict(slope=conf.conversion.slope, offset=conf.conversion.intercept)
+        pc.configure(d)
 
 
 class SteererCollection(Device, MultiplexerSetMixin):
@@ -28,16 +51,8 @@ class SteererCollection(Device, MultiplexerSetMixin):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # put default value from repo
-        for name in self.power_converter_names.get():
-            conf = repo.get(name)
-            pc = getattr(self.power_converters, name)
-            d = dict(slope=conf.conversion.slope, offset=conf.conversion.intercept)
-            pc.configure(d)
+        configure_scaling_power_converters(self.power_converters, repo)
 
 
 if __name__ == "__main__":
     steerer_muxer = SteererCollection(name="sc")
-    steerer_muxer
-    print("steerer_muxer imported")
